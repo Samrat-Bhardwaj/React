532bb5427354a537992fa99fde1c9ca8d406f7f5
SAM Bhardwaj <bsamrat47@gmail.com>
2021-01-21 16:46:21 +0530

    21/1/2021

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from "react";
+import ReactDom from "react-dom";
 
-function Greeting(){
-  return <h3>lmao okey</h3>
+function QuoteList() {
+  return (
+    <section>
+    <Quote></Quote>
+    <Quote></Quote>
+    <Quote></Quote>
+    </section>
+  );
 }
 
-ReactDom.render(<Greeting/>, document.getElementById('root'));
+const Quote = () => {
+  return (
+    <section>
+    <Image/>
+    <Author/>
+    </section>
+  )
+};
+
+const Image = () => {
+  return 
+  (<img
+    src="https://2uc5db1v032h5jwcm2gal07i-wpengine.netdna-ssl.com/wp-content/uploads/legacy/sites/default/files/new2.jpg"
+    alt=""
+  />);
+);
+
+const Author = () => {
+  return <h3>Samrat</h3>;
+};
+
+ReactDom.render(<QuoteList />, document.getElementById("root"));
